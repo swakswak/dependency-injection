91e733e257463fa882a80f38a45aaa2e0dc9ad87
swakswak <hschoi0702@gmail.com>
2022-02-27 13:34:09 +0900

    PackageScanner의 주요 로직 try-catch-resources로 변경

--- src/toy/swak/PackageScanner.java
+++ src/toy/swak/PackageScanner.java
@@ -1,9 +1,11 @@
 package toy.swak;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.annotation.Annotation;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 /**
@@ -23,35 +25,38 @@ class PackageScanner {
     }
 
     public void scan(String packagePath) {
-        InputStream resourceStream = ClassLoader.getSystemClassLoader()
+        try (InputStream resourceStream = ClassLoader.getSystemClassLoader()
                 .getResourceAsStream(packagePath.replace(".", "/"));
-        InputStreamReader reader = new InputStreamReader(resourceStream);
-        BufferedReader bufferedReader = new BufferedReader(reader);
-
-        Stream<String> lines = bufferedReader.lines();
-        lines.forEach(line -> {
-            if (isClass(line)) {
-                scanClasses(packagePath, line);
-            } else {
-                scanPackages(packagePath, line);
-            }
-        });
+             InputStreamReader reader = new InputStreamReader(Objects.requireNonNull(resourceStream));
+             BufferedReader bufferedReader = new BufferedReader(reader);
+        ) {
+            Stream<String> lines = bufferedReader.lines();
+            lines.forEach(line -> {
+                if (this.isClass(line)) {
+                    this.scanClasses(packagePath, line);
+                } else {
+                    this.scanPackages(packagePath, line);
+                }
+            });
+            resourceStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void scanPackages(String packagePath, String line) {
-        System.out.println("Package: " + line);
+        System.out.println("- Package: " + line);
         scan(packagePath + "." + line);
     }
 
     private void scanClasses(String packagePath, String line) {
         try {
-            System.out.println("Class: " + line);
+            System.out.println("- Class: " + line);
             String classPath = packagePath + "." + line.replace(".class", "");
             Class<?> klass = Class.forName(classPath);
             for (Annotation annotation : klass.getAnnotations()) {
-                System.out.println("Annotation: " + annotation.annotationType().getSimpleName());
+                System.out.println("\t- @" + annotation.annotationType().getSimpleName());
             }
-
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
